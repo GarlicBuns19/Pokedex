6e1af555df4e7361502f2d985a47c1006e6370a3
GarlicBuns19 <planterx26@gmail.com>
2022-06-20 06:31:14 +0100

    help

--- pokedex.js
+++ pokedex.js
@@ -1,26 +1,45 @@
 const row = document.querySelector('.row')
 
-async function pokemonData() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/'
-    try{
-        let pokeCollection =  await fetch(url);
-        return await pokeCollection.json();
-    }catch(error){
-        console.log(error)
-    }
-}
-console.log(pokemonData())
-async function pokeCards() {
-    let pokemon = await pokemonData()
-    pokemon.results.forEach(item => {
-        `
-        <div class="col-md-4">
-                <div class="card" style="width: 21rem;">
-                        <h5 class="card-title">${item.name}</h5>
-                    </div>
-                </div> 
-            </div>
-        `
-    })
-}
-row.innerHTML = pokeCards()
+let pokeUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+fetch(pokeUrl)
+.then(response => response.json())
+.then(data => {
+    console.log('Success', data)
+}).catch((error) => {
+    console.log('Not Successful', error)
+})
+
+
+
+
+
+
+
+
+
+
+// async function pokemonData() {
+//     let url = 'https://pokeapi.co/api/v2/pokemon/'
+//     try{
+//         let pokeCollection =  await fetch(url);
+//         return await pokeCollection.json();
+//     }catch(error){
+//         console.log(error)
+//     }
+// }
+// console.log(pokemonData())
+// async function pokeCards() {
+//     let pokemon = await pokemonData()
+//     pokemon.results.forEach(item => {
+//         `
+//         <div class="col-md-4">
+//                 <div class="card" style="width: 21rem;">
+//                         <h5 class="card-title">${item.name}</h5>
+//                     </div>
+//                 </div> 
+//             </div>
+//         `
+//     })
+// }
+// row.innerHTML = pokeCards()
