5e8d250800197eab300218fac004781f67ba33e8
GarlicBuns19 <planterx26@gmail.com>
2022-06-21 05:29:11 +0100

    help

--- pokedex.js
+++ pokedex.js
@@ -2,31 +2,32 @@ const row = document.querySelector('.row')
 
 const pokeUrl = 'https://pokeapi.co/api/v2/pokemon/'
 
-// async function getData(){
-//     let data = await fetch(pokeUrl)
-//     .then(data => data.json())
-//     .then(data => {
-//         data.results.forEach((n) => {
-//             row.innerHTML += `<h1>${n.name}</h1>`
-//             // use the += to show all data
-//         })
-//     })
-//     // .then(data => console.log(data.results[0].name))
-//     try{
-//         console.log('Success')
-//     }catch(error){
-//         console.log('error')
-//     }
-    
-// }
-// getData();
-
-fetch(pokeUrl).then(
-    res => res.json() 
-).then(
-    res => {
-        res.results.forEach((e) => {
-            console.log(e.name)
+async function getData(){
+    let data = await fetch(pokeUrl)
+    .then(data => data.json())
+    .then(data => {
+        data.results.forEach((n) => {
+            row.innerHTML += `<h1>${n.name}</h1>`
+            row.innerHTML += `<h1>${n.url}</h1>`
+            // use the += to show all data
         })
+    })
+    // .then(data => console.log(data.results[0].name))
+    try{
+        console.log('Success')
+    }catch(error){
+        console.log('error')
     }
-)
+    
+}
+getData();
+
+// Pokemon Names
+// fetch(pokeUrl).then(
+//     res => res.json() 
+// ).then(res => {
+//         res.results.forEach((e) => {
+//             console.log(e.name);
+//             console.log(e.url);
+//         })}
+// )
