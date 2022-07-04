ce63e227c63f55aa505f780ab9aa72bc923c34a4
GarlicBuns19 <planterx26@gmail.com>
2022-07-04 18:47:00 +0100

    lets go

--- pokedex.js
+++ pokedex.js
@@ -18,18 +18,18 @@ async function getData(url){
             // use the += to show all data
             // = shows last data
         })
+        // For next and previous pokemon
         if(data.previous){
             next.innerHTML += `<button onclick="getData('${data.previous}')">Previous Pokemon</button>`;
         }
         next.innerHTML += `<button onclick="getData('${data.next}')">Next Pokemon</button>`;
-        // next.innerHTML += `<button onclick="getData('${data.previous}')">Previous Pokemon</button>`;
     })
-    // .then(data => console.log(data.results[0].name))
     try{
         console.log('Success')
     }catch(error){
         console.log('error')
     }
+    return data;
 };
 getData(pokeUrl);
 // =======================================================================================================================
@@ -40,6 +40,7 @@ function cardCreate(){
     card.innerHTML = `<div class="col-md-4">
          <div class="card" style="width: 21rem;">
             <div id="poke-Img"> </div>
+            <div id="pokeType"> </div>
             <div class="card-body"> </div>
          </div>
      </div>`
@@ -52,6 +53,13 @@ function pokemonUrl(url) {
     fetch(url)
     .then(res => res.json())
     .then(pokemon => {
+            pokemon.types.forEach((typeName) => {
+                document.getElementById('pokeType').innerHTML += `
+                <p class="card-text">
+                    ${typeName.type.name}
+                </p>
+                `
+            })
             // document.querySelector('#poke-Img').innerHTML += `<img src="${pokemon.sprites.front_default}" class="card-img-top img-fluid" alt="Pokemon Pic">`
             document.querySelector('#poke-Img').innerHTML += `<img src="${pokemon.sprites.other['official-artwork'].front_default}" class="card-img-top img-fluid" alt="Pokemon Pic">`
             document.querySelector('.card-body').innerHTML += `
@@ -59,47 +67,45 @@ function pokemonUrl(url) {
                     ${pokemon.name}
                 </h5>
                 <p class="card-text">
-                    ${pokemon.types[0].type.name}
-                </p>
-                <p class="card-text card-text-1">
-                    ${pokemon.types[1].type.name}
-                </p>
-                <p class="card-text">
                     ${pokemon.abilities[0].ability.name}
                 </p>
             `
-            if(`${pokemon.types[0].type.name}` == 'normal'){
-                document.querySelector('.card-text').style.backgroundColor = 'gray';
-            }else if (`${pokemon.types[0].type.name}` == 'fire'){
-                document.querySelector('.card-text').style.backgroundColor = 'red';
-            }else if (`${pokemon.types[0].type.name}` == 'water',`${pokemon.types[1].type.name}` == 'water'){
-                document.querySelector('.card-text').style.backgroundColor = 'blue';
-            }else if (`${pokemon.types[0].type.name}` == 'grass'){
-                document.querySelector('.card-text').style.backgroundColor = 'green';
-            }else if (`${pokemon.types[0].type.name}` == 'electric'){
-                document.querySelector('.card-text').style.backgroundColor = 'yellow';
-            }else if (`${pokemon.types[0].type.name}` == 'ice'){
-                document.querySelector('.card-text').style.backgroundColor = 'lightblue';
-            }else if (`${pokemon.types[0].type.name}` == 'fighting'){
-                document.querySelector('.card-text').style.backgroundColor = 'brown';
-            }else if (`${pokemon.types[0].type.name}` == 'poison'){
-                document.querySelector('.card-text').style.backgroundColor = 'purple';
-            }else if (`${pokemon.types[0].type.name}` == 'ground'){
-                document.querySelector('.card-text').style.backgroundColor = 'sandybrown';
-            }else if (`${pokemon.types[0].type.name}` == 'flying'){
-                document.querySelector('.card-text').style.backgroundColor = 'rgb(170, 195, 237)';
-            }
-            else{
-                document.querySelector('.card-text').style.backgroundColor = 'pink';
-            }
-            if(`${pokemon.types[1].type.name}` == 'poison'){
-                document.querySelector('.card-text-1').style.backgroundColor = 'purple';
-            }
-    })
-    .then(pokemon => {
-        pokemon.types.forEach((typeName) => {
-
-        })
+            
+                
+            
+            // <p class="card-text">
+            //         ${pokemon.types[0].type.name}
+            //     </p>
+            //     <p class="card-text card-text-1">
+            //         ${pokemon.types[1].type.name}
+            //     </p>
+            // if(`${pokemon.types[0].type.name}` == 'normal'){
+            //     document.querySelector('.card-text').style.backgroundColor = 'gray';
+            // }else if (`${pokemon.types[0].type.name}` == 'fire'){
+            //     document.querySelector('.card-text').style.backgroundColor = 'red';
+            // }else if (`${pokemon.types[0].type.name}` == 'water',`${pokemon.types[1].type.name}` == 'water'){
+            //     document.querySelector('.card-text').style.backgroundColor = 'blue';
+            // }else if (`${pokemon.types[0].type.name}` == 'grass'){
+            //     document.querySelector('.card-text').style.backgroundColor = 'green';
+            // }else if (`${pokemon.types[0].type.name}` == 'electric'){
+            //     document.querySelector('.card-text').style.backgroundColor = 'yellow';
+            // }else if (`${pokemon.types[0].type.name}` == 'ice'){
+            //     document.querySelector('.card-text').style.backgroundColor = 'lightblue';
+            // }else if (`${pokemon.types[0].type.name}` == 'fighting'){
+            //     document.querySelector('.card-text').style.backgroundColor = 'brown';
+            // }else if (`${pokemon.types[0].type.name}` == 'poison'){
+            //     document.querySelector('.card-text').style.backgroundColor = 'purple';
+            // }else if (`${pokemon.types[0].type.name}` == 'ground'){
+            //     document.querySelector('.card-text').style.backgroundColor = 'sandybrown';
+            // }else if (`${pokemon.types[0].type.name}` == 'flying'){
+            //     document.querySelector('.card-text').style.backgroundColor = 'rgb(170, 195, 237)';
+            // }
+            // else{
+            //     document.querySelector('.card-text').style.backgroundColor = 'pink';
+            // }
+            // if(`${pokemon.types[1].type.name}` == 'poison'){
+            //     document.querySelector('.card-text-1').style.backgroundColor = 'purple';
+            // }
     })
 };
 // =======================================================================================================================
